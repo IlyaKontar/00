e0c1599cd191786e737b125d54797475e4afcb20
IlyaKontar <40755032+IlyaKontar@users.noreply.github.com>
2018-07-04 08:12:13 +0300

    Add files via upload

---
+++ ProgramTriangle.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Triangle
+{
+    class ProgramTriangle
+    {
+        static void Main(string[] args)
+        {
+            
+           Console.WriteLine("Введіть 1 число.");
+            double y=Convert.ToDouble(Console.ReadLine());           
+           Console.WriteLine("Введіть 2 число.");
+           double z=Convert.ToDouble(Console.ReadLine());
+           Console.WriteLine("Введіть 3 число.");
+           double c=Convert.ToDouble(Console.ReadLine()); 
+           if (y>z && y>c){
+               if (y>z+c) {
+                   Console.WriteLine("Output:True");
+                }
+                else{
+                     Console.WriteLine("Output:False"); 
+                }
+            }
+           else{
+               if(y==z){
+                   if(y==c){
+                   Console.WriteLine("Output:True");
+                   }
+                else{
+                    if(c<y+z){Console.WriteLine("Output:True");
+                    }
+                 else{
+                     Console.WriteLine("Output:False");
+                 }
+                }
+               }
+            else{
+                if(z<y+c){Console.WriteLine("Output:True");
+                    }
+                 else{
+                     if (z>y && z>c){
+               if (z>y+c) {
+                   Console.WriteLine("Output:True");
+               }
+           else{
+              Console.WriteLine("Output:False"); 
+           }
+           
+               
+           
+           }
+           else{
+               if(z==y){
+                   if(z==c){
+                   Console.WriteLine("Output:True");
+                   }
+                else{
+                    if(c<y+z){Console.WriteLine("Output:True");
+                    }
+                 else{
+                     Console.WriteLine("Output:False");
+                 }
+                }
+               }
+            else{
+                if(y<z+c){Console.WriteLine("Output:True");
+                    }
+                 else{
+                     if (c>z && c>y){
+               if (c>z+y) {
+                   Console.WriteLine("Output:True");
+               }
+           else{
+              Console.WriteLine("Output:False"); 
+           }
+           
+               
+           
+           }
+           else{
+               if(c==z){
+                   if(y==c){
+                   Console.WriteLine("Output:True");
+                   }
+                else{
+                    if(y<c+z){Console.WriteLine("Output:True");
+                    }
+                 else{
+                     Console.WriteLine("Output:False");
+                 }
+                }
+               }
+            else{
+                if(z<y+c){Console.WriteLine("Output:True");
+                    }
+                 else{
+                     Console.WriteLine("Output:False");
+            }
+           }
+            }
+           } 
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
